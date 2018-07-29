9c21550b9c00e48941c6201ed3168dbd22f9b573
Seth Morabito <web@loomcom.com>
2018-07-29 11:27:18 -0700

    Use SDL2 for drawing

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,25 @@
 /// <https://www.gnu.org/licenses/>.
 ///
 
+extern crate rand;
+extern crate sdl2;
+
 pub mod vec3;
 pub mod ray;
 pub mod hittable;
 pub mod camera;
-
-extern crate rand;
 
 use rand::prelude::*;
 use vec3::Vec3;
 use ray::Ray;
 use hittable::*;
 use camera::Camera;
+use std::{thread, time};
+
+use sdl2::rect::Rect;
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 
 fn color(r: &Ray, world: &World, depth: i32) -> Vec3 {
     let hit: Option<Hit> = world.hit(r, 0.001, std::f32::MAX);
@@ -51,9 +58,23 @@ fn color(r: &Ray, world: &World, depth: i32) -> Vec3 {
 }
 
 fn main() {
-    const NX: i32 = 1600;
-    const NY: i32 = 800;
-    const NS: i32 = 100;
+    const NX: u32 = 400;
+    const NY: u32 = 200;
+    const NS: u32 = 100;
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("Rust Raytracer", NX, NY)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+    let texture_creator = canvas.texture_creator();
+
+    let mut texture = texture_creator.create_texture_streaming(
+        PixelFormatEnum::RGB24, NX, NY + 1).unwrap();
 
     let mut rng = thread_rng();
 
@@ -76,35 +97,61 @@ fn main() {
 
     let camera: Camera = Camera::default();
 
-    // Print the PPM header
-    println!("P3\n{} {}\n255", NX, NY);
+    // Display the data in the SDL window
+    for j in (0..=NY).rev() {
+        texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+            for i in 0..NX {
 
-    // Print the data
-    for j in (0..=(NY-1)).rev() {
-        for i in 0..NX {
-            let mut col: Vec3 = Vec3::new(0.0, 0.0, 0.0);
+                let x = (NY-j) as usize;
+                let y = i as usize;
 
-            for _ in 0..NS {
-                let ir: f32 = rng.gen();
-                let jr: f32 = rng.gen();
-                let u: f32 = (i as f32 + ir) / NX as f32;
-                let v: f32 = (j as f32 + jr) / NY as f32;
-                let r: Ray = camera.get_ray(u, v);
-                col += color(&r, &world, 0);
+                let mut col: Vec3 = Vec3::new(0.0, 0.0, 0.0);
+
+                for _ in 0..NS {
+                    let ir: f32 = rng.gen();
+                    let jr: f32 = rng.gen();
+                    let u: f32 = (i as f32 + ir) / NX as f32;
+                    let v: f32 = (j as f32 + jr) / NY as f32;
+                    let r: Ray = camera.get_ray(u, v);
+                    col += color(&r, &world, 0);
+                }
+
+                col /= NS as f32;
+
+                // Adjust gamma
+                col.e[0] = col.e[0].sqrt();
+                col.e[1] = col.e[1].sqrt();
+                col.e[2] = col.e[2].sqrt();
+
+                let ir: u8 = (255.99 * col.r()) as u8;
+                let ig: u8 = (255.99 * col.g()) as u8;
+                let ib: u8 = (255.99 * col.b()) as u8;
+
+                let offset = x*pitch + y*3;
+
+                buffer[offset] = ir;
+                buffer[offset + 1] = ig;
+                buffer[offset + 2] = ib;
             }
+        }).unwrap();
 
-            col /= NS as f32;
+        canvas.copy(&texture, None, Some(Rect::new(0, 0, NX, NY))).unwrap();
+        canvas.present();
+    }
 
-            // Adjust gamma
-            col.e[0] = col.e[0].sqrt();
-            col.e[1] = col.e[1].sqrt();
-            col.e[2] = col.e[2].sqrt();
+    let mut event_pump = sdl_context.event_pump().unwrap();
 
-            let ir: i32 = (255.99 * col.r()) as i32;
-            let ig: i32 = (255.99 * col.g()) as i32;
-            let ib: i32 = (255.99 * col.b()) as i32;
-
-            println!("{} {} {}", ir, ig, ib);
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
         }
+
+        thread::sleep(time::Duration::from_millis(10));
     }
 }
